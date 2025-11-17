2bae9d5f6a7a8debaa2f8681067667c37ba1b8d3
JHONATHANUNI <jfuni@uniboyaca.edu.co>
2025-11-17 09:03:38 -0500

    Headder completado siguiente paso catalogo de marcas de autos

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,9 +3,25 @@ import { Outlet } from "react-router-dom";
 export default function Layout() {
   return (
     <div className="app-container">
+
       {/* HEADER */}
-      <header className="main-header">
-        <h1 className="logo">CARS LUXURY</h1>
+      <header className="header">
+        <div className="menu container">
+          <a href="/" className="logo">LUXURY SPORT VEHICLES</a>
+
+          <input type="checkbox" id="menu" />
+          <label htmlFor="menu">
+            <img src="/img/menu.png" className="menu-icono" alt="Menú" />
+          </label>
+
+          <nav className="navbar">
+            <ul>
+              <li><a href="/redes">Redes sociales</a></li>
+              <li><a href="/noticias">Noticias</a></li>
+              <li><a href="/diseñadores">Diseñadores</a></li>
+            </ul>
+          </nav>
+        </div>
       </header>
 
       {/* CONTENIDO */}
@@ -17,6 +33,7 @@ export default function Layout() {
       <footer className="main-footer">
         <p>© 2025 CARS LUXURY - Todos los derechos reservados</p>
       </footer>
+
     </div>
   );
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,35 @@
 export default function HomePage() {
-  return <h2>Landing Page funcionando</h2>;
+  return (
+    <div>
+      <section className="header-content container">
+        <div className="header-txt">
+          <h1>
+            Descubre el <span>Mundo</span>
+            <br />
+            de los Hipercoches
+          </h1>
+
+          <p>
+            ¡Bienvenido a la página donde los sueños sobre ruedas se hacen
+            realidad!
+            <br />
+            Explora y disfruta de los automóviles más increíbles del mundo.
+            <br />
+            Admira los coches más lujosos, potentes y exclusivos que existen.
+          </p>
+
+          <div className="buttons">
+            <a href="#" className="btn-1">
+              Información
+            </a>
+            <a href="#" className="btn-1">
+              Historia
+            </a>
+          </div>
+        </div>
+      </section>
+
+      {/* Aquí luego va la sección de catálogo */}
+    </div>
+  );
 }
